119b8252bfbfdde2da17deb5cbd3c2cc6401da88
Guilherme Gomes <guilhermegfoliveira2020@gmail.com>
2024-06-10 18:33:07 -0300

    Listagem dos blogs já escritos na página de hom funcionando

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {MDBRow, MDBCol, MDBContainer, MDBTypography} from 'mdb-react-ui-kit';
+import { toast } from 'react-toastify';
+import Blogs from '../components/Blogs';
 
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadBlogsData();
+  }, [])
+
+
+  const loadBlogsData = async () => {
+    const response = await axios.get('http://localhost:5000/blogs');
+
+    if (response.status === 200){
+      setData(response.data);
+    } else{
+      toast.error('Algo deu errado!');
+    }
+  };
+
+  console.log('data', data);
+
+  const handleDelete = () => {};
+
+  const excerpt = (str) => {
+    return str.length > 50 ? str.substring(0, 50) + "..." : str;
+  };
+
+
   return (
-    <div>
-        <h2>Home</h2>
-    </div>
+    <>
+      <MDBRow> 
+        {data.length === 0 && (
+          <MDBTypography 
+            className='text-center mb-0' 
+            tag='h2'
+          >
+            Nenhum Blog Encontrado
+          </MDBTypography>
+        )}
+        <MDBCol>
+          <MDBContainer>
+            <MDBRow>
+              {data && data.map((item, index) =>(
+                <Blogs
+                  key={index}
+                  {...item}
+                  excerpt={excerpt}
+                  handleDelete={handleDelete}
+                />
+              ))}
+            </MDBRow>
+          </MDBContainer>
+        </MDBCol>
+
+
+      </MDBRow>
+    </>
   )
 }
 
