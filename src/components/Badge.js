e4359287cc57c31975de4e261b2215112468e260
Guilherme Gomes <guilherme.oliveira@gmgroupbrasil.com.br>
2024-06-18 20:31:01 -0300

    Componente sendo criado

---
+++ src/components/Badge.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Badge = ({children, styleInfo}) => {
+    const colorKey = {
+        Academia: 'danger',
+        Esportes: 'dark',
+        GastronomiaCulinaria: 'warning',
+        Moda: 'primary',
+        Tecnologia: 'info',
+        Viagem: 'success',
+    }
+    
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Badge
