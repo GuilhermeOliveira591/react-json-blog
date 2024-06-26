c30bbbe44a6d4c6868f492615548ca12a25a467c
Guilherme Gomes <guilhermegfoliveira2020@gmail.com>
2024-06-26 09:39:25 -0300

    Projeto finalizado

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import {MDBContainer, MDBTypography} from 'mdb-react-ui-kit';
 
 const About = () => {
   return (
-    <div>
-        <h2> About </h2>
+    <div style={{marginTop: '100px'}}>
+        <MDBContainer>
+          <MDBTypography note noteColor='primary'>
+            Somos um blog onde nosso público pode encontrar notícias relacionadas
+            as mais diferentes categorias como Viagem, Gastronomia, Esportes, Academia, Tecnologia e Moda
+            (Ainda em construção!)
+          </MDBTypography>
+        </MDBContainer>
     </div>
   )
 }
